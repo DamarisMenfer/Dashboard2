7c33a19e62c36e755216245f3a9f234dbccf8073
mendesfe <damaris.menfer@gmail.com>
2019-01-18 18:07:37 +0100

    script change

--- src/main/webapp/ajax/javaScript.js
+++ src/main/webapp/ajax/javaScript.js
@@ -1,12 +1,39 @@
-function loadDoc() {
-	console.log("hi");
+function getState(id) {
 	$.ajax({
 		//envoi de la requête
 		method: "GET", 
-		url: "http://localhost:8888/RestProject/webapi/myresources/services/Light/data/state",
-		dataType : "xml",
+		url: "http://localhost:8888/RestProject/webapi/myresources/services/Light/data/state/"+id,
+		dataType : "text",
 		success: function( data ) {
-			Console.log("hi");
+			Console.log(data);
+		}
+	});
+}
+
+function changeState(id){
+	if (id == )
+}
+
+function turnOn(id) {
+	$.ajax({
+		//envoi de la requête
+		method: "POST", 
+		url: "http://localhost:8888/RestProject/webapi/myresources/services/Light/data/state/on/"+id,
+		dataType : "text",
+		success: function( data ) {
+			Console.log(data);
+		}
+	});
+}
+
+function turnOff(id) {
+	$.ajax({
+		//envoi de la requête
+		method: "POST", 
+		url: "http://localhost:8888/RestProject/webapi/myresources/services/Light/data/state/off/"+id,
+		dataType : "text",
+		success: function( data ) {
+			Console.log(data);
 		}
 	});
 }
